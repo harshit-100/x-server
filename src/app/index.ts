9e203377ef4710714c235c03038d0b775c6ca6ee
harshit sharma <harshitsharma22462@gmail.com>
2023-10-17 22:18:17 +0530

    added prisma

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import bodyParser from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "../clients/db";
 
 export async function initServer() {
   const app = express();
+
   app.use(bodyParser.json());
 
   const graphqlServer = new ApolloServer({
